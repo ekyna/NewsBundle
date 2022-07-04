c23132e66837a07ee5dc389f7a59e6f419bbfdb2
Etienne Dauvergne <contact@ekyna.com>
2022-07-04 10:05:13 +0200

    [News] PHP 8.1

--- Repository/NewsRepositoryInterface.php
+++ Repository/NewsRepositoryInterface.php
@@ -4,12 +4,16 @@ declare(strict_types=1);
 
 namespace Ekyna\Bundle\NewsBundle\Repository;
 
+use Ekyna\Bundle\NewsBundle\Model\NewsInterface;
+use Ekyna\Component\Resource\Repository\ResourceRepositoryInterface;
 use Ekyna\Component\Resource\Repository\TranslatableRepositoryInterface;
 
 /**
  * Interface NewsRepositoryInterface
  * @package Ekyna\Bundle\NewsBundle\Repository
  * @author  Étienne Dauvergne <contact@ekyna.com>
+ *
+ * @implements ResourceRepositoryInterface<NewsInterface>
  */
 interface NewsRepositoryInterface extends TranslatableRepositoryInterface
 {
